4729addb8cd5173baaecdb6b126912e518262f87
Muhammad Umar Mansyur <umar@MacBook-Air-Muhammad.local>
2022-11-30 18:45:57 +0700

    Login with google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 const { user_game } = require("../models");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 module.exports = {
   register: async (req, res, next) => {
     try {
@@ -36,11 +36,11 @@ module.exports = {
         status: 201,
         message: "Data berhasil ditambahkan",
         data: {
-          username: user.username
+          username: user.username,
         },
       });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   login: async (req, res, next) => {
@@ -53,12 +53,12 @@ module.exports = {
             version: "1.0",
           },
           meta: {
-            author: 'Muhammad Umar Mansyur',
-            copyright: '2022 ~ BE JavaScript Binar Academy'
+            author: "Muhammad Umar Mansyur",
+            copyright: "2022 ~ BE JavaScript Binar Academy",
           },
           status: 404,
-          message: 'User not found'
-        })
+          message: "User not found",
+        });
       }
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (!isPasswordValid) {
@@ -67,36 +67,38 @@ module.exports = {
             version: "1.0",
           },
           meta: {
-            author: 'Muhammad Umar Mansyur',
-            copyright: '2022 ~ BE JavaScript Binar Academy'
+            author: "Muhammad Umar Mansyur",
+            copyright: "2022 ~ BE JavaScript Binar Academy",
           },
           status: 401,
-          message: 'Password is not valid'
-        })
+          message: "Password is not valid",
+        });
       }
       let payload = {
         id: user.id,
         username: user.username,
-        password: user.password
-      }
-      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+        password: user.password,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
       return res.status(200).json({
         jsonapi: {
           version: "1.0",
         },
         meta: {
-          author: 'Muhammad Umar Mansyur',
-          copyright: '2022 ~ BE JavaScript Binar Academy'
+          author: "Muhammad Umar Mansyur",
+          copyright: "2022 ~ BE JavaScript Binar Academy",
         },
         status: 200,
-        message: 'Login success',
+        message: "Login success",
         data: {
           id: user.id,
-          accessToken: token
-        }
-      })
+          accessToken: token,
+        },
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   },
   saya: (req, res, next) => {
@@ -107,17 +109,67 @@ module.exports = {
           version: "1.0",
         },
         meta: {
-          author: 'Muhammad Umar Mansyur',
-          copyright: '2022 ~ BE JavaScript Binar Academy'
+          author: "Muhammad Umar Mansyur",
+          copyright: "2022 ~ BE JavaScript Binar Academy",
         },
         status: 200,
-        message: 'Success',
-        data: user
-      }
+        message: "Success",
+        data: user,
+      };
       return res.status(200).json(result);
     } catch (error) {
       next(error);
     }
   },
-  
+  loginGoogle: async (req, res, next) => {
+    try {
+      const code = req.query.code;
+      if (!code) {
+        const url = googleOauth2.generateAuthURL();
+        return res.redirect(url);
+      }
+
+      await googleOauth2.setCredentials(code);
+      const { data } = await googleOauth2.getUserData();
+      // kolom password jika login menggunakan google jadikan tempat email
+      let userExist = await User.findOne({ where: { email: data.password } });
+      let exist = false;
+      if (!userExist) {
+        userExist = await user_game.create({
+          username: data.name,
+          password: data.email,
+        });
+      } else {
+        exist = true;
+        userExist = await User.update(
+          {
+            name: data.name,
+            password: data.email,
+          },
+          { where: { password: data.password }, returning: true }
+        );
+      }
+      return res.status(200).json({
+        data: exist == true ? userExist[1][0] : userExist,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  loginFacebook: async (req, res, next) => {
+    try {
+      const code = req.query.code;
+      if (!code) {
+        const url = facebookOauth2.generateAuthURL();
+        return res.redirect(url);
+      }
+      const access_token = await facebookOauth2.getAccessToken(code);
+      const userInfo = await facebookOauth2.getUserInfo(access_token);
+      console.log(userInfo.picture.data.url);
+      res.send(userInfo);
+    } catch (err) {
+      next(err);
+    }
+  },
 };
